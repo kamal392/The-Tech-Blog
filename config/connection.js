1241cb7c9911fad988f097e3c5bfcbb56936acab
kamal kishore <kamal.kishore981@gmail.com>
2022-11-03 22:15:09 -0400

    Modified connection.js file; removed unused scripts id dsashboard view.

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 // create connection to db, whether local on on heroku using jawsdb
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize("tech_blog_db", "root", "Kishore@123", {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
